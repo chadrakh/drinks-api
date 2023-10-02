356cc02b4eac0d0c6ad18a7eab8b6e8949632932
chadrakh <chadrak@outlook.com>
2023-10-02 13:53:01 +0100

    Add GET CofeeLover Test

--- src/test/java/tr/drinksapi/controller/CoffeeControllerTests.java
+++ src/test/java/tr/drinksapi/controller/CoffeeControllerTests.java
@@ -1,5 +1,28 @@
 package tr.drinksapi.controller;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@AutoConfigureMockMvc
+@SpringBootTest
 class CoffeeControllerTests {
+
+    @Autowired
+    MockMvc mockMvcController;
+
+    @Test
+    public void getCoffeeLover() throws Exception {
+        String expected = "I like coffee!";
+
+        mockMvcController.perform(
+                MockMvcRequestBuilders.get("/cofee-lover"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string(expected));
+    }
 
 }
