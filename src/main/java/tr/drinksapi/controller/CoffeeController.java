aacd630b95d9225d32f2f9629bc55930a844ba3a
chadrakh <chadrak@outlook.com>
2023-10-02 14:25:15 +0100

    Implement GET Coffee Endpoint

--- src/main/java/tr/drinksapi/controller/CoffeeController.java
+++ src/main/java/tr/drinksapi/controller/CoffeeController.java
@@ -1,7 +1,11 @@
 package tr.drinksapi.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import tr.drinksapi.model.Coffee;
 
 @RestController
 public class CoffeeController {
@@ -9,5 +13,22 @@ public class CoffeeController {
     @GetMapping(value = "/coffeelover")
     public String coffeeLover() {
         return "I like coffee!";
+    }
+
+    @GetMapping(value = "/coffee")
+    public ResponseEntity<Coffee> getCoffee(@RequestParam(required = false) String name) {
+        Long id = 1L;
+        String coffeeName = "latte";
+
+        if(name != null) {
+            coffeeName = name;
+        }
+
+        Coffee coffee = Coffee.builder()
+                .id(id)
+                .name(coffeeName)
+                .build();
+
+        return new ResponseEntity<>(coffee, HttpStatus.OK);
     }
 }
