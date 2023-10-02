660b3a973815c1770994f827df5658a584e80c9b
chadrakh <chadrak@outlook.com>
2023-10-02 13:59:09 +0100

    Amend coffeelover endpoint

--- src/main/java/tr/drinksapi/controller/CoffeeController.java
+++ src/main/java/tr/drinksapi/controller/CoffeeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CoffeeController {
 
-    @GetMapping(value = "/coffee-lover")
+    @GetMapping(value = "/coffeelover")
     public String coffeeLover() {
         return "I like coffee!";
     }
